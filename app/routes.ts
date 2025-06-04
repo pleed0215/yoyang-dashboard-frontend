7f8f22bfebe937747b538e055a94b690c2b67cfb
pleed0215 <pleed@naver.com>
2025-06-04 17:12:26 +0900

    Integrate dashboard layout and routing configuration updates

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,10 @@
-import { type RouteConfig, index, route } from '@react-router/dev/routes';
+import { type RouteConfig, index, route, layout, prefix } from '@react-router/dev/routes';
 
 export default [
   index('./index.tsx'),
   route('/login', 'features/auth/login.tsx'),
   route('/logout', 'features/auth/logout.tsx'),
+  layout('features/common/layouts/layout.tsx', [
+    ...prefix('/dashboard', [index('features/common/pages/dashboard-index.tsx')]),
+  ]),
 ] satisfies RouteConfig;
